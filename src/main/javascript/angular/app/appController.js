211fee54315ac9cf9fd0f8570163b2f79e0a1e21
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-12-11 21:18:38 +0100

    angular2

---
+++ src/main/javascript/angular/app/appController.js
@@ -0,0 +1,6 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.controller('NinjaController', ['$scope', function($scope) {
+  $scope.message = "hey y'all";
+  $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
+}]);
